22ab09bcae9a1432bd4fd82e27960bde33a770b9
lsflp <lipeluiis@hotmail.com>
2016-11-16 12:01:20 -0200

    Arrumando

--- Testes/trace.c
+++ Testes/trace.c
@@ -7,17 +7,18 @@ int main () {
     int i, j, linhas, n, pi, ti, t0, tf, b;
     printf("%d %d %d %d\n", total, virtual, s, p);
     srand((unsigned)time(NULL));
-    linhas = 1+(rand()%100);       // número de processos (entre 1 e 100)
+    linhas = 1+(rand()%100);                       // número de processos (entre 1 e 100)
     for (i = 0; i < linhas; i++) {
-        t0 = 1 + (rand()%5);       // tempo inicial (entre 1 e 5)
-        tf = 1 + (rand()%10) + t0; // delta t + t0 (No máximo 10 seg de execução)
-        b = 1 + (rand()%128);       // total de memória usado (entre 1 e 128)
+        t0 = 1 + (rand()%5);                       // tempo inicial (entre 1 e 5)
+        tf = 1 + (rand()%10) + t0;                 // delta t + t0 (No máximo 10 seg de execução)
+        b = 64 + (rand()%64);                      // total de memória usado (entre 64 e 128)
         printf("%d proc%d %d %d ", t0, i, tf, b);
-        n = 1 + (rand()%15);                // número de pags e instantes que ele as acessa (entre 1 e 15)
+        n = (tf-t0);                               // número de pags e instantes que ele as acessa (1 por segundo)
         for (j = 0; j < n; j++) {
-            pi = 1 + (rand()%16);           // o numero da pag que ele acessa (entre 1 e 16 = 64/4) 
-            ti = t0 + (rand()%(tf - t0));   // o tempo que ele acessa (entre t0 e tf)
-            printf("%d %d ", pi, ti);
+            pi = 1 + (rand()%16);                  // o numero da pag que ele acessa (entre 1 e 16 = 64/4) 
+            ti = j + t0;
+            if (j == 0 || rand() > (0.3*RAND_MAX)) // Prob de 70% de nesse instante ele mexer em uma pagina
+                printf("%d %d ", pi, ti);          // Mexe em uma pelo menos.
         }
         printf("\n");
     }
